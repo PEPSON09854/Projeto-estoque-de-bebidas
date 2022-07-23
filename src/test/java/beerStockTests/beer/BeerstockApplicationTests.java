e1ad264bf7faa4fad4274f191dac25c756a477ff
Pepson Anderson <pepsonanderson@hotmail.com>
2022-07-23 17:04:58 -0300

    projeto estoque de bebidas spring boot

---
+++ src/test/java/beerStockTests/beer/BeerstockApplicationTests.java
@@ -0,0 +1,13 @@
+package beerStockTests.beer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerstockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
